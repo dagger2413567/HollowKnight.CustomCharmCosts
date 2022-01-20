6fe6ceec5098088855c598ab450259bfd8af2726
IncognitoWerewolf <dagger2413567@gmail.com>
2022-01-20 05:42:58 -0500

    Fixed Names

--- CustomCharmCosts/CustomCharmCosts.cs
+++ CustomCharmCosts/CustomCharmCosts.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Linq;
-using System.Reflection;
-using HutongGames.PlayMaker;
-using HutongGames.PlayMaker.Actions;
-using JetBrains.Annotations;
-using Modding;
+﻿using Modding;
 using UnityEngine;
 
 namespace CustomCharmCosts
