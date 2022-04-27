725b0af1f57ec0bcab8632da639403a4e609f6a3
IncognitoWerewolf <dagger2413567@gmail.com>
2022-04-27 05:56:54 -0400

    Added Deep focus and Quick Focus with some logic. Made the unbreakables only have the reduction after becoming unbreakable.

--- CustomCharmCosts/CustomCharmCosts.cs
+++ CustomCharmCosts/CustomCharmCosts.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 namespace CustomCharmCosts
 {
     [UsedImplicitly]
-    public class CustomCharmCosts : Mod, IGlobalSettings<GlobalModSettings>
+    public class CustomCharmCosts : Mod, ITogglableMod, IGlobalSettings<GlobalModSettings>
     {
         public CustomCharmCosts() : base("Custom Charm Costs") { }
 
@@ -17,22 +17,29 @@ namespace CustomCharmCosts
 
         public override string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
+        public override int LoadPriority() => 10;
+
         public override void Initialize()
         {
             Log("Initializing");
-            
+
             ModHooks.GetPlayerIntHook += OnInt;
         }
 
         private int OnInt(string intName, int orig) => intName switch
         {
-            // Gathering, Wayfayers, Soul Catcher, Soul Eater, Dashmaster, Quick Slash, SprintMaster, Grimmchild
-            "charmCost_01" => _settings.GSwarmCost,
-            "charmCost_02" => _settings.WayfairerCost,
+            // Gathering, Wayfayers, Soul Catcher, Soul Eater, Dashmaster, Quick Slash, SprintMaster, Grimmchild, Heart/Greed/Strength
+            "charmCost_1" => _settings.GSwarmCost,
+            "charmCost_2" => _settings.WayfairerCost,
+            "charmCost_7" => PlayerData.instance.GetBool("gotCharm_34") ? 0 :_settings.QuickFocusCost,
             "charmCost_20" => _settings.CatcherCost,
             "charmCost_21" => _settings.EaterCost,
+            "charmCost_23" => PlayerData.instance.fragileHealth_unbreakable ? _settings.UHeartCost : 2,
+            "charmCost_24" => PlayerData.instance.fragileGreed_unbreakable ? _settings.UGreedCost : 2,
+            "charmCost_25" => PlayerData.instance.fragileStrength_unbreakable ? _settings.UStrengthCost : 3,
             "charmCost_31" => _settings.DashmasterCost,
             "charmCost_32" => _settings.QuickCost,
+            "charmCost_34" => PlayerData.instance.GetBool("gotCharm_7") ? 0 : _settings.DeepFocusCost,
             "charmCost_37" => _settings.SprintCost,
             "charmCost_40" => _settings.GrimmCost,
 
--- CustomCharmCosts/GlobalModSettings.cs
+++ CustomCharmCosts/GlobalModSettings.cs
@@ -7,11 +7,16 @@ namespace CustomCharmCosts
     {
         public int GSwarmCost = 0;
         public int WayfairerCost = 0;
+        public int QuickFocusCost = 2;
         public int CatcherCost = 1;
         public int EaterCost = 3;
         public int DashmasterCost = 1;
         public int QuickCost = 1;
+        public int DeepFocusCost = 3;
         public int SprintCost = 0;
         public int GrimmCost = 1;
+        public int UHeartCost = 1;
+        public int UGreedCost = 1;
+        public int UStrengthCost = 2;
     }
 }
